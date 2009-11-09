a896bfae783a580f92aabe8a33f7e3cda2e9ee0e
Christoph Zeheter <chris.zehetner@gmail.com>
2009-11-09 10:56:18 +0100

    issue #1 - interface for the issue tracker

---
+++ tita-issuetracker-integration/src/main/java/at/ac/tuwien/ifs/tita/issuetracker/enums/IssueStatus.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.ifs.tita.issuetracker.enums;
+
+
+/**
+ * Describes the possibilities for issue states
+ * @author Christoph
+ *
+ */
+public enum IssueStatus {
+	NEW, ASSIGNED, CLOSED
+}
---
+++ tita-issuetracker-integration/src/main/java/at/ac/tuwien/ifs/tita/issuetracker/enums/ViewState.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.ifs.tita.issuetracker.enums;
+
+/**
+ * Describes the possibilities for the view state
+ * @author Christoph
+ *
+ */
+public enum ViewState {
+	PUBLIC, PRIVATE, ANY
+}
