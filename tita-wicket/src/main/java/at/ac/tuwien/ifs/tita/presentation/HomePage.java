637de692bdb13aec7cc71fc16afb2f63556246a4
Herbert Brunner <herbert@brahms.(none)>
2009-11-15 11:50:22 +0100

    hello world is working Signed-off-by: Herbert Brunner <herbert@brahms.(none)>

--- tita-wicket/src/main/java/at/ac/tuwien/ifs/tita/presentation/HomePage.java
+++ tita-wicket/src/main/java/at/ac/tuwien/ifs/tita/presentation/HomePage.java
@@ -3,19 +3,18 @@ package at.ac.tuwien.ifs.tita.presentation;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.springframework.transaction.annotation.Transactional;
 
-import at.ac.tuwien.ifs.tita.datasource.dao.IBaseDAO;
 import at.ac.tuwien.ifs.tita.datasource.domain.Role;
 import at.ac.tuwien.ifs.tita.datasource.exception.TitaDAOException;
+import at.ac.tuwien.ifs.tita.datasource.service.IUserService;
 
 /**
  * Homepage of Hello World Application.
  */
 public class HomePage extends WebPage {
 
-    @SpringBean(name="roleDAO")
-    private IBaseDAO<Role> dummy;
+    @SpringBean(name="userService")
+    private IUserService service;
     
     public HomePage() {
         displayHello();
@@ -24,14 +23,13 @@ public class HomePage extends WebPage {
     /**
      * Puts "Hello World" in DB and reads it afterwards.
      */
-    @Transactional
     private void displayHello(){
         Role helloWorld = initPage();
         
         try {
-            Role temp = dummy.save(helloWorld);
+            Role temp = service.saveRole(helloWorld);
             helloWorld = null;
-            helloWorld = dummy.getById(Role.class, temp.getId());
+            helloWorld = service.getRoleById(temp.getId());
             add(new Label("message", helloWorld.getDescription()));
         } catch (TitaDAOException e) {
             add(new Label("message", "Couldn't read data from DB."));
