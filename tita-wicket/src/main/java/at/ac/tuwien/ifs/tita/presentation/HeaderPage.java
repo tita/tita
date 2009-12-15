1b6b8a3e4ad4efea688af595b26704821713b6be
Rene Kernegger <rene.kernegger@tele2.at>
2009-12-15 19:19:46 +0100

    iss#48, grouped links, added searchfield Signed-off-by: Rene Kernegger <rene.kernegger@tele2.at>

--- tita-wicket/src/main/java/at/ac/tuwien/ifs/tita/presentation/HeaderPage.java
+++ tita-wicket/src/main/java/at/ac/tuwien/ifs/tita/presentation/HeaderPage.java
@@ -1,6 +1,9 @@
 package at.ac.tuwien.ifs.tita.presentation;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.TextField;
 
 /**
  * Class for dynamic header information.
@@ -13,5 +16,16 @@ public class HeaderPage extends BasePage {
     public HeaderPage() {
         String username = "User1";
         add(new Label("showUser", "Signed in as " + username));
+
+        final WebMarkupContainer timeConsumergroup = new WebMarkupContainer("timeConsumerGroup");
+        final WebMarkupContainer timeControllergroup = new WebMarkupContainer("timeControllerGroup");
+        final WebMarkupContainer administratorGroup = new WebMarkupContainer("administratorGroup");
+        add(timeConsumergroup);
+        add(timeControllergroup);
+        add(administratorGroup);
+
+        TextField<String> searchField = new TextField<String>("searchField");
+        add(searchField);
+        add(new Button("searchButton"));
     }
 }
