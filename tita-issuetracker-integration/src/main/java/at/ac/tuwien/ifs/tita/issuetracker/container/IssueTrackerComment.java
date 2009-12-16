ad2a2632c1e60ffb492798b7fad28b5fe14af040
Christoph Zeheter <chris.zehetner@gmail.com>
2009-12-16 11:50:43 +0100

    licenses and tests skipped from tita-business
    Signed-off-by: Christoph Zeheter <chris.zehetner@gmail.com>

--- tita-issuetracker-integration/src/main/java/at/ac/tuwien/ifs/tita/issuetracker/container/IssueTrackerComment.java
+++ tita-issuetracker-integration/src/main/java/at/ac/tuwien/ifs/tita/issuetracker/container/IssueTrackerComment.java
@@ -3,7 +3,9 @@
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
+   
        http://www.apache.org/licenses/LICENSE\-2.0
+       
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,7 +29,7 @@ import at.ac.tuwien.ifs.tita.issuetracker.interfaces.ICommentTrackable;
  */
 public class IssueTrackerComment implements ICommentTrackable {
     private Date creationTime;
-    private long id;
+    private final long id;
     private Date lastChange;
     private String reporter;
     private String text;
@@ -45,32 +47,32 @@ public class IssueTrackerComment implements ICommentTrackable {
 
     /** {@inheritDoc} */
     public Date getCreationTime() {
-        return this.creationTime;
+        return creationTime;
     }
 
     /** {@inheritDoc} */
     public Long getId() {
-        return this.id;
+        return id;
     }
 
     /** {@inheritDoc} */
     public Date getLastChange() {
-        return this.lastChange;
+        return lastChange;
     }
 
     /** {@inheritDoc} */
     public String getReporter() {
-        return this.reporter;
+        return reporter;
     }
 
     /** {@inheritDoc} */
     public String getText() {
-        return this.text;
+        return text;
     }
 
     /** {@inheritDoc} */
     public ViewState getViewState() {
-        return this.viewState;
+        return viewState;
     }
 
     /** {@inheritDoc} */
--- tita-wicket/src/main/java/at/ac/tuwien/ifs/tita/presentation/effort/evaluation/timecontroller/MultipleProjectsView.java
+++ tita-wicket/src/main/java/at/ac/tuwien/ifs/tita/presentation/effort/evaluation/timecontroller/MultipleProjectsView.java
@@ -1,3 +1,18 @@
+/**
+   Copyright 2009 TiTA Project, Vienna University of Technology
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+   
+       http://www.apache.org/licenses/LICENSE\-2.0
+       
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+  
+ */
 package at.ac.tuwien.ifs.tita.presentation.effort.evaluation.timecontroller;
 
 import java.util.Arrays;
@@ -24,7 +39,7 @@ import at.ac.tuwien.ifs.tita.presentation.controls.dropdown.SelectOption;
  * 
  */
 public class MultipleProjectsView extends HeaderPage {
-    private SelectOption selectedTimespan;
+    private final SelectOption selectedTimespan;
 
     public MultipleProjectsView() {
         Form<Object> form = new Form<Object>("multipleProjectsForm");
